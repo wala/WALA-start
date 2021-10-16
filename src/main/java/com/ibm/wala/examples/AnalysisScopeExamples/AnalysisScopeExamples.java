269c7f33e32b1a554dfc27f47f80d9147a86b62e
Jerry <xuveda@gmail.com>
2021-10-15 17:17:12 -0700

    initial commit

--- src/main/java/com/ibm/wala/examples/AnalysisScopeExamples/AnalysisScopeExamples.java
+++ src/main/java/com/ibm/wala/examples/AnalysisScopeExamples/AnalysisScopeExamples.java
@@ -1,4 +1,16 @@
 package com.ibm.wala.examples.AnalysisScopeExamples;
 
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.util.config.FileOfClasses;
+import com.ibm.wala.util.config.AnalysisScopeReader;
+
+import java.io.IOException;
+
+import static com.ibm.wala.util.config.AnalysisScopeReader.makeJavaBinaryAnalysisScope;
+
 public class AnalysisScopeExamples {
+    AnalysisScope makeAnalysisScope(String classPath) throws IOException{
+       return makeJavaBinaryAnalysisScope(classPath, null);
+    }
 }
