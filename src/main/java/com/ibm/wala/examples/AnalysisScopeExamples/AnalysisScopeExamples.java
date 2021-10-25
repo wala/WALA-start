9626f973825b67695c8d4801917656c62ad11419
Jerry <xuveda@gmail.com>
2021-10-25 14:23:35 -0700

    hopefully finished the scope file example implementation

--- src/main/java/com/ibm/wala/examples/AnalysisScopeExamples/AnalysisScopeExamples.java
+++ src/main/java/com/ibm/wala/examples/AnalysisScopeExamples/AnalysisScopeExamples.java
@@ -5,9 +5,12 @@ import com.ibm.wala.ipa.callgraph.AnalysisScope;
 import com.ibm.wala.util.config.FileOfClasses;
 import com.ibm.wala.util.config.AnalysisScopeReader;
 
+import java.io.File;
 import java.io.IOException;
-
 import com.ibm.wala.util.config.AnalysisScopeReader;
+
+//for more information, please check out https://github.com/wala/WALA/wiki/Analysis-Scope
+
 
 public class AnalysisScopeExamples {
     /**
@@ -15,7 +18,18 @@ public class AnalysisScopeExamples {
      * @return AnaylsisScope object created by makeJavaBinaryAnalysisScope
      * @throws IOException
      */
-    AnalysisScope makeAnalysisScope(String classPath) throws IOException{
-       return AnalysisScopeReader.makeJavaBinaryAnalysisScope(classPath, null);
+    AnalysisScope makeAnalysisScope(String classPath) throws IOException {
+        return AnalysisScopeReader.makeJavaBinaryAnalysisScope(classPath, null);
+    }
+
+    /**
+     *
+     * @param classPath Location of a scope file in string form
+     * @param exceptionFile location of an exception file
+     * @return return an analysis scope object
+     * @throws IOException
+     */
+    AnalysisScope makeAnalysisScope(String classPath, String exceptionFile) throws IOException{
+        return AnalysisScopeReader.makeJavaBinaryAnalysisScope(classPath, new File(exceptionFile));
     }
 }
