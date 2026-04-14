45fc1ac5bcf3ea2e9c79afa48e3fbfaa0040676a
Ben Liblit <liblit@acm.org>
2026-04-14 15:39:34 -0400

    Convert Gradle build logic from Groovy to Kotlin (#26)
    Kotlin is the recommended and default language for modern Gradle build
    scripts.  Users of this package may be more familiar with Kotlin than
    with Groovy.  Even if they are not, a sample package like this one
    should use recommended best practices wherever possible to set a good
    example for others to build upon.

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+  `java-library`
+  application
+  eclipse
+  id("com.diffplug.spotless") version "8.4.0"
+}
+
+java.toolchain.languageVersion = JavaLanguageVersion.of(17)
+
+version = "0.2"
+
+tasks.jar {
+  manifest.attributes(
+      "Implementation-Title" to "WALA Start",
+      "Implementation-Version" to archiveVersion,
+  )
+}
+
+repositories {
+  mavenCentral()
+  mavenLocal()
+}
+
+dependencies {
+  val walaVersion = "1.6.9"
+
+  implementation("com.ibm.wala:com.ibm.wala.shrike:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.util:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.core:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.cast:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.cast.js:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.cast.js.rhino:${walaVersion}")
+  // Duplicate implementation in original build.gradle
+  // implementation("com.ibm.wala:com.ibm.wala.cast.js:${walaVersion}")
+  // implementation("com.ibm.wala:com.ibm.wala.cast.js.rhino:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.cast.java:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.cast.java.ecj:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.dalvik:${walaVersion}")
+  implementation("com.ibm.wala:com.ibm.wala.scandroid:${walaVersion}")
+  testImplementation("junit:junit:4.+")
+}
+
+application.mainClass =
+    project.findProperty("mainClass") as? String?
+        ?: "com.ibm.wala.examples.drivers.PDFTypeHierarchy"
+
+spotless {
+  java { googleJavaFormat() }
+  kotlinGradle { ktfmt() }
+}
